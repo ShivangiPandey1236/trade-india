d8181291c27e180312c6c3ffc01a556362800337
ShivangiPandey1236 <sp765101@gmail.com>
2026-05-19 22:22:20 +0530

    Setup the telwind css

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      <h1>hello Shivangi, welcome to Trade-India platform</h1>
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 shadow-sm p-8 bg-white rounded-xl">
+        hello Shivangi, welcome to Trade-India platform
+      </h1>
     </div>
   )
 }
